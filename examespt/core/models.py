4f7de988da7a96b4e8173b6bfddad9418694915f
Telmo Guerra <tguerra@confidencialimobiliario.com>
2018-07-20 23:08:30 +0100

    first commit

---
+++ examespt/core/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+
+class BaseClass(models.Model):
+    name = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
+
+class University(BaseClass):
+    pass
+
