55eef2181c1570f30a418fe556267d8a3c1ecc2d
Telmo Guerra <tguerra@confidencialimobiliario.com>
2018-08-31 17:56:39 +0100

    import courseunits

--- examespt/core/migrations/0002_data.py
+++ examespt/core/migrations/0002_data.py
@@ -10,7 +10,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 
 def data(apps, schema_editor):
     data_districts(apps)
-    data_universities(apps)
     data_institutes(apps)
     data_season(apps)
 
@@ -18,7 +17,7 @@ def data(apps, schema_editor):
 def data_districts(apps):
     District = apps.get_model('core', 'District')
 
-    with open(os.path.join(BASE_DIR, 'data/districts.csv')) as district_data:
+    with open(os.path.join(BASE_DIR, 'data/core/data/districts.csv')) as district_data:
         data = csv.reader(district_data)
         for district in data:
             District.objects.create(name=district[0])
@@ -37,46 +36,44 @@ def data_season(apps):
         Season.objects.create(id=id, name=name)
 
 
-def data_universities(apps):
-    University = apps.get_model('core', 'University')
+def data_institutes(apps):
+    Institute = apps.get_model('core', 'Institute')
     District = apps.get_model('core', 'District')
-    with open(os.path.join(BASE_DIR, 'data/institute_course.csv')) as institute_course_data:
+    with open(os.path.join(BASE_DIR, 'data/core/data/institute_course.csv')) as institute_course_data:
         data = csv.reader(institute_course_data)
         for univ, course in data:
-            with open(os.path.join(BASE_DIR, 'data/districts.csv')) as district_data:
+            with open(os.path.join(BASE_DIR, 'data/core/data/districts.csv')) as district_data:
                 d_data = csv.reader(district_data)
                 for district in d_data:
                     if district[0] in univ:
                         dist_obj = District.objects.filter(name=district[0]).get()
-                        split = univ.split('-')
-                        if len(split) == 2:
-                            try:
-                                University.objects.get(name=split[0])
-                            except ObjectDoesNotExist:
-                                University.objects.create(name=split[0], location=dist_obj)
+                        try:
+                            Institute.objects.get(name=univ)
+                        except ObjectDoesNotExist:
+                            Institute.objects.create(name=univ, district=dist_obj)
 
 
-def data_institutes(apps):
-    University = apps.get_model('core', 'University')
-    Institute = apps.get_model('core', 'Institute')
-    with open(os.path.join(BASE_DIR, 'data/institute_course.csv')) as institute_course_data:
-        data = csv.reader(institute_course_data)
-        for univ, course in data:
-            split = univ.split('-')
-            if len(split) == 2:
-                try:
-                    un_obj = University.objects.get(name=split[0])
-                except ObjectDoesNotExist:
-                    un_obj = None
-                try:
-                    Institute.objects.get(name=split[1])
-                except ObjectDoesNotExist:
-                    Institute.objects.create(name=split[1], university=un_obj)
-            elif len(split) == 1:
-                try:
-                    Institute.objects.get(name=split[0])
-                except ObjectDoesNotExist:
-                    Institute.objects.create(name=split[0], university=None)
+# def data_institutes(apps):
+#     University = apps.get_model('core', 'University')
+#     Institute = apps.get_model('core', 'Institute')
+#     with open(os.path.join(BASE_DIR, 'data/institute_course.csv')) as institute_course_data:
+#         data = csv.reader(institute_course_data)
+#         for univ, course in data:
+#             split = univ.split('-')
+#             if len(split) == 2:
+#                 try:
+#                     un_obj = University.objects.get(name=split[0])
+#                 except ObjectDoesNotExist:
+#                     un_obj = None
+#                 try:
+#                     Institute.objects.get(name=split[1])
+#                 except ObjectDoesNotExist:
+#                     Institute.objects.create(name=split[1], university=un_obj)
+#             elif len(split) == 1:
+#                 try:
+#                     Institute.objects.get(name=split[0])
+#                 except ObjectDoesNotExist:
+#                     Institute.objects.create(name=split[0], university=None)
 
 
 class Migration(migrations.Migration):
